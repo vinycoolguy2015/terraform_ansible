d3aa342809e4bf02a4f3ccfef08264003473a16e
Cloud User <centos@ip-172-31-40-7.ec2.internal>
2020-07-06 06:14:51 +0000

    script modification

--- ques4/script.sh
+++ ques4/script.sh
@@ -1,17 +1,19 @@
 #! /bin/bash
 
-yum install -y mysql git awscli jq
+#yum install -y mysql git awscli jq
+yum install -y git awscli jq
 
-mysql -u root -pTest1086# -h application-rds.chynuosgviuy.us-east-1.rds.amazonaws.com -e "create database test;use test;CREATE TABLE users (id int(11) NOT NULL auto_increment,name varchar(100) NOT NULL,age int(3) NOT NULL,email varchar(100) NOT NULL,PRIMARY KEY (id));"
+#mysql -u root -pTest1086# -h application-rds.chynuosgviuy.us-east-1.rds.amazonaws.com -e "create database test;use test;CREATE TABLE users (id int(11) NOT NULL auto_increment,name varchar(100) NOT NULL,age int(3) NOT NULL,email varchar(100) NOT NULL,PRIMARY KEY (id));"
 
 yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 yum install -y nodejs
 
-git clone https://github.com/chapagain/nodejs-mysql-crud
-#rds_hostname=`aws rds describe-db-instances --region us-east-1 |jq [.DBInstances[0].Endpoint.Address][0]| tr -d '"'`
-rds_status=`aws rds describe-db-instances --region us-east-1 |jq [.DBInstances[0].DBInstanceStatus][0]| tr -d '"'`
-if [ $rds_status != "available" ]; then  sleep 14m;fi
+#git clone https://github.com/chapagain/nodejs-mysql-crud
+git https://github.com/vinycoolguy2015/nodejs-mysql-crud.git
+rds_hostname=`aws rds describe-db-instances --region us-east-1 |jq [.DBInstances[0].Endpoint.Address][0]| tr -d '"'`
+#rds_status=`aws rds describe-db-instances --region us-east-1 |jq [.DBInstances[0].DBInstanceStatus][0]| tr -d '"'`
+if [ $rds_hostname == "null" ]; then  sleep 5m;fi
 rds_hostname=`aws rds describe-db-instances --region us-east-1 |jq [.DBInstances[0].Endpoint.Address][0]| tr -d '"'`
 
 cd nodejs-mysql-crud
