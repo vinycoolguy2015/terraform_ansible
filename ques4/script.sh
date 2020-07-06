23fb4442859cf7d4328985f7abc473b923cf60e7
vinycoolguy2015 <vinayakpandeyit@gmail.com>
2020-07-06 14:35:34 +0800

    Update script.sh

--- ques4/script.sh
+++ ques4/script.sh
@@ -7,7 +7,7 @@ curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 yum install -y nodejs
 
 #git clone https://github.com/chapagain/nodejs-mysql-crud
-git https://github.com/vinycoolguy2015/nodejs-mysql-crud.git
+git clone https://github.com/vinycoolguy2015/nodejs-mysql-crud.git
 rds_hostname=`aws rds describe-db-instances --region us-east-1 |jq [.DBInstances[0].Endpoint.Address][0]| tr -d '"'`
 #rds_status=`aws rds describe-db-instances --region us-east-1 |jq [.DBInstances[0].DBInstanceStatus][0]| tr -d '"'`
 if [ $rds_hostname == "null" ]; then  sleep 5m;fi
